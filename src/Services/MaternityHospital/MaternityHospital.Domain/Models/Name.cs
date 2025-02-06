5d78a6e18fcd04c625e9a157588a10417551a552
Uladzislau Tsitou <capt.Hoffmann@gmail.com>
2025-02-06 21:01:35 +0300

    Patient and name models were moved in domain layer #6

---
+++ src/Services/MaternityHospital/MaternityHospital.Domain/Models/Name.cs
@@ -0,0 +1,23 @@
+﻿namespace MaternityHospital.Domain.Models;
+
+/// <summary>
+/// Name model
+/// </summary>
+public class Name
+{
+    /// <summary>
+    /// Use propery
+    /// </summary>
+    public string? Use { get; set; }
+
+    /// <summary>
+    /// Given property
+    /// </summary>
+    public string[]? Given { get; set; }
+
+#nullable disable
+    /// <summary>
+    /// Family property (required)
+    /// </summary>
+    public string Family { get; set; }
+}
---
+++ src/Services/MaternityHospital/MaternityHospital.Domain/Models/Patient.cs
@@ -0,0 +1,34 @@
+﻿using MaternityHospital.Domain.Enums;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MaternityHospital.Domain.Models;
+
+#nullable disable
+/// <summary>
+/// Patient model
+/// </summary>
+public class Patient
+{
+    /// <summary>
+    /// Id property
+    /// </summary>
+    [BsonId]
+    public Guid Id { get; set; }
+    /// <summary>
+    /// Name property
+    /// </summary>
+    public Name Name { get; set; }
+    /// <summary>
+    /// Gender property
+    /// </summary>
+    public Gender Gender { get; set; }
+    /// <summary>
+    /// BirthDate property (required)
+    /// </summary>
+    public DateTime BirthDate { get; set; }
+    /// <summary>
+    /// IsActive property
+    /// </summary>
+    public bool Active { get; set; }
+}
+
