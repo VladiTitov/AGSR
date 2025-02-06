9583a67cbc40eb2ec1d82c8d449e718da6e0ed68
Uladzislau Tsitou <capt.Hoffmann@gmail.com>
2025-02-06 21:03:13 +0300

    OpenApi configuration was added in swagger configuration #6

--- src/Services/MaternityHospital/MaternityHospital.Api/Configuration/Swagger/SwaggerConfiguration.cs
+++ src/Services/MaternityHospital/MaternityHospital.Api/Configuration/Swagger/SwaggerConfiguration.cs
@@ -1,16 +1,31 @@
-﻿namespace MaternityHospital.Api.Configuration.Swagger;
+﻿using Microsoft.OpenApi.Models;
+
+namespace MaternityHospital.Api.Configuration.Swagger;
 
 internal static class SwaggerConfiguration
 {
     internal static IServiceCollection RegisterSwagger(this IServiceCollection services)
         => services
             .AddEndpointsApiExplorer()
-            .AddSwaggerGen();
+            .AddSwaggerGen(opt =>
+            {
+                var basePath = AppContext.BaseDirectory;
+
+                opt.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Maternity hospital api",
+                    Description = "Test task for AGSR company"
+                });
+
+                var xmlPath = Path.Combine(basePath, "MaternityHospital.Api.xml");
+                opt.IncludeXmlComments(xmlPath);
+            });
 
     internal static IApplicationBuilder ConfigureSwagger(this IApplicationBuilder app)
         => app
             .UseSwagger()
             .UseSwaggerUI(c => c.SwaggerEndpoint(
                 url: "/swagger/v1/swagger.json", 
-                name: "MaternityHospital.Api"));
+                name: "v1"));
 }
